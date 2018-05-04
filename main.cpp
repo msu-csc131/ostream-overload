e81b4d539fae7895b70dc0d7f2fa2a375a340c2f
Mr. Daehn <jdaehn@missouristate.edu>
2018-05-04 11:41:42 -0500

    Update main to use FriendlyType

--- main.cpp
+++ main.cpp
@@ -7,13 +7,17 @@
 
 #include <cstdlib>
 #include <iostream>
+#include "FriendlyType.h"
 #include "SomeType.h"
 
 int main(int argc, char** argv)
 {
-	SomeType someObject{15};
+	FriendlyType friendlyTypeObject{20};
+	SomeType someTypeObject{15};
 
-	std::cout << "Hello... " << someObject << std::endl;
+	std::cout << "Hello... " << friendlyTypeObject << std::endl;
+	std::cout << "Hello... " << someTypeObject << std::endl;
+
 	return EXIT_SUCCESS;
 }
 
